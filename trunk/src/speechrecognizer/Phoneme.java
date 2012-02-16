868d48b50f9dc55c9e70e06c8ea7f508d9433d4c
i.mynah@gmail.com <i.mynah@gmail.com@5c3ac1e4-8c29-b8e4-cada-c6937851699b>
2012-02-16 05:07:00 +0000

    bugfixed all the parsing stuff.. now going to bugfix viterbi algorithm

--- trunk/src/speechrecognizer/Phoneme.java
+++ trunk/src/speechrecognizer/Phoneme.java
@@ -14,16 +14,21 @@ public class Phoneme {
 	
 	public Phoneme(String datastring){
 		String[] parts = datastring.split("<");
-		name = parts[0].replace("\n", "").replace("\\", "");
+		name = parts[0].replace("\n", "").replace("\\", "").replace("\"", "");
+		name = name.trim();
 		
-		float[][] means     = new float[5][5];
-		float[][] variances = new float[5][5];
+		float[][] means     = new float[5][39];
+		float[][] variances = new float[5][39];
         
-		int index = 0;
+		int index = -1;
         for(String part : parts) {
-        	if(part.startsWith("MEAN")) {
-            	part.replace("MEAN> 39\n", "");
-            	String[] subparts = part.split(" ");
+        	if(part.startsWith("STATE")) {
+        		index++;
+        	}
+        	else if(part.startsWith("MEAN")) {
+            	part = part.replace("MEAN> 39\n", "");
+            	part = part.trim();
+            	String[] subparts = part.split(" ");            	
             	int subindex = 0;
             	for(String x : subparts) {
             		means[index][subindex] = Float.valueOf(x.trim()).floatValue();
@@ -31,7 +36,8 @@ public class Phoneme {
             	}
             }
             else if (part.startsWith("VARIANCE")) {
-            	part.replace("VARIANCE> 39\n", "");
+            	part = part.replace("VARIANCE> 39\n", "");
+            	part = part.trim();
             	String[] subparts = part.split(" ");
             	int subindex = 0;
             	for(String x : subparts) {
@@ -40,11 +46,13 @@ public class Phoneme {
             	}
             }
             else if (part.startsWith("TRANSP")) {
-            	part.replace("TRANSP> 5\n", "");
-            	String[] lines = part.split(" ");
+            	part = part.replace("TRANSP> 5\n", "");
+            	part = part.trim();
+            	String[] lines = part.split("\n");
             	int row = 0;
             	int column = 0;
             	for(String line : lines) {
+            		line = line.trim();
             		String[] cells = line.split(" ");
             		for(String cell : cells){
             			transition_probabilities[row][column] = Float.valueOf(cell.trim()).floatValue();
@@ -54,9 +62,8 @@ public class Phoneme {
             		row++;
             	}
             }
-        	index++;
         }           
-
+        
         state2 = new State(means[0], variances[0], name + "2", this);
         state3 = new State(means[1], variances[1], name + "3", this);
         state4 = new State(means[2], variances[2], name + "4", this);
